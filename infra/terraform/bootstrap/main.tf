9161559e71b64451ca43aec573eecae9e57c9844
imcherry5778 <263946457+imcherry5778@users.noreply.github.com>
2026-09-24 02:08:18 +0900

    infra(identity): GitHub Actions federated identity 추가

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "state" {
+resource "azurerm_resource_group" "bootstrap" {
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "state" {
 
 resource "azurerm_storage_account" "state" {
   name                = var.storage_account_name
-  resource_group_name = azurerm_resource_group.state.name
-  location            = azurerm_resource_group.state.location
+  resource_group_name = azurerm_resource_group.bootstrap.name
+  location            = azurerm_resource_group.bootstrap.location
 
   account_kind             = "StorageV2"
   account_tier             = "Standard"
@@ -38,4 +38,28 @@ resource "azurerm_storage_container" "state" {
   name                  = var.container_name
   storage_account_id    = azurerm_storage_account.state.id
   container_access_type = "private"
+}
+
+resource "azurerm_user_assigned_identity" "github_deploy" {
+  name                = var.deployment_identity_name
+  resource_group_name = azurerm_resource_group.bootstrap.name
+  location            = azurerm_resource_group.bootstrap.location
+  tags                = var.tags
+}
+
+resource "azurerm_federated_identity_credential" "github_azure_demo" {
+  name                      = "github-azure-demo"
+  user_assigned_identity_id = azurerm_user_assigned_identity.github_deploy.id
+  audience                  = ["api://AzureADTokenExchange"]
+  issuer                    = "https://token.actions.githubusercontent.com"
+  subject                   = var.github_oidc_subject
+}
+
+resource "azurerm_role_assignment" "github_state" {
+  scope                            = azurerm_storage_account.state.id
+  role_definition_name             = "Storage Blob Data Contributor"
+  principal_id                     = azurerm_user_assigned_identity.github_deploy.principal_id
+  principal_type                   = "ServicePrincipal"
+  skip_service_principal_aad_check = true
+  description                      = "Allow approved GitHub Actions workflows to read and write Terraform state."
 }
