f01e2a38409363ed0e41cb540aefce1212eb492e
imcherry5778 <263946457+imcherry5778@users.noreply.github.com>
2026-09-24 15:18:13 +0900

    infra(terraform): foundation provider 계약 추가

---
+++ infra/terraform/foundation/provider.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+
+  storage_use_azuread             = true
+  resource_provider_registrations = "none"
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "foundation" {
+  name = var.foundation_resource_group_name
+}
